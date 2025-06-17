55f66ca7ab5db1e0902fbf96b17e6b9b66ce35bc
serhhan <serhanelmali@gmail.com>
2025-06-17 16:18:22 +0300

    mend

---
+++ src/routes/peers/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import PeerList from '../../components/PeerList.svelte';
+	
+	let { children } = $props();
+</script>
+
+<div class="flex h-full gap-4 w-full">
+	<div class="w-80 flex-shrink-0">
+		<PeerList />
+	</div>
+	{@render children()}
+</div>
